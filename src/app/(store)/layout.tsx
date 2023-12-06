8f2b293c8cdfd653aa0e236950290503bf408671
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2023-12-05 21:16:51 -0300

    chore: cart context

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@/components/header'
+import { CartProvider } from '@/contexts/cart-context'
 import { ReactNode } from 'react'
 
 interface StoreLayout {
@@ -7,9 +8,11 @@ interface StoreLayout {
 
 export default function StoreLayout({ children }: StoreLayout) {
   return (
-    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 px-8 py-8">
-      <Header />
-      {children}
-    </div>
+    <CartProvider>
+      <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 px-8 py-8">
+        <Header />
+        {children}
+      </div>
+    </CartProvider>
   )
 }
